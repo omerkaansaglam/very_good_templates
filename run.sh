abc0969e8859804a674bed4d8ed97f5f6e104adf
ÖMER KAAN SAĞLAM <kaansaglam1406@gmail.com>
2024-10-31 17:29:27 +0300

    update sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 dart pub global activate mason_cli &&
 echo "It may prompt you for your password to delete the .mason-cache folder." &&
-sudo rm -rf ~/.mason-cache && echo "Cache cleared."
-# mason add -g arteria_flutter_template --git-url https://github.com/omerkaansaglam/very_good_templates &&
-# mason make arteria_flutter_template
+sudo rm -rf ~/.mason-cache && echo "Cache cleared." &&
+mason add -g arteria_flutter_template --git-url https://github.com/omerkaansaglam/very_good_templates &&
+mason make arteria_flutter_template
